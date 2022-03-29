f085ccdc99b2edd0912a837cccaa8e531bed1113
Ziban89 <ziban89@tut.by>
2022-03-29 21:30:31 +0300

    fix: some fixes

--- src/main/java/ru/clevertec/copyutil/CopyUtil.java
+++ src/main/java/ru/clevertec/copyutil/CopyUtil.java
@@ -1,7 +1,6 @@
 package ru.clevertec.copyutil;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -16,17 +15,30 @@ public class CopyUtil {
         }
         int sourceSize = source.size();
 
-        List<Object> dest = new LinkedList<>();
-        for(int i = 0; i < 5; i++) dest.add(new Object());
-        int oldDestSize = dest.size();
-        copy(source, dest);
-        int newDestSize = dest.size();
-        System.out.println(String.format("Source size: %d, dest size: %d, dest size after copy: %d", sourceSize, oldDestSize, newDestSize));
+        List<Object> objectsDest = new LinkedList<>();
+        for(int i = 0; i < 5; i++) objectsDest.add(new Object());
+        int oldDestSize = objectsDest.size();
+        copy(source, objectsDest);
+        int newDestSize = objectsDest.size();
+        printMessage(sourceSize, oldDestSize, newDestSize);
+
+        List<Number> numbersDest = new ArrayList<>();
+        for(int i = 0; i < 7; i++) numbersDest.add(i);
+
+        oldDestSize = numbersDest.size();
+        copy(source, numbersDest);
+        newDestSize = numbersDest.size();
+
+        printMessage(sourceSize, oldDestSize, newDestSize);
    }
 
     static <T> void copy(List<? extends T> source, List<? super T> dest) {
         for(T t: source) {
             dest.add((t));
         }
+    }
+
+    static void printMessage(int sourceSize, int oldDestSize, int newDestSize){
+        System.out.printf("Source size: %d, dest size: %d, dest size after copy: %d%n", sourceSize, oldDestSize, newDestSize);
     }
 }
